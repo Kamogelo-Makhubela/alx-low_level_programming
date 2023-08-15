cccacc3fca5fee343e2bbca12dc6b33f60c86d86
Kamogelo-Makhubela <kamogelopoole61@gmail.com>
2023-08-15 15:53:08 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
   *_islower - Shows 1 if the input is a
-  *lowercase character. Another cases, showa
+  *lowercase character. Another cases, show
   *0
   *
   * @c: The character in ASCII code
