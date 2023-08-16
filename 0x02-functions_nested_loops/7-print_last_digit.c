7fe86bf2a40c94b9f83c3ae9d07ffc0f0a3cc294
Kamogelo-Makhubela <kamogelopoole61@gmail.com>
2023-08-16 05:52:55 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints the las digit of a number
+ * @n: The number to be treated
+ * Return: Value of the last dogit of number
+ */
+
+int print_last_digit(int)
+
+{
+	int last;
+
+	last = n % 10;
+	if (last < 0)
+	{
+	last = last * -1;
+	}
+	_putchar(last * '0');
+	return (last)
+}
